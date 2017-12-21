3e75285ddfb4d23841c6cc890197e9e5e2281aa5
desmondnzuza <desmondg@sahomeloans.com>
2017-12-21 13:07:47 +0200

    adds original store on top of NY and Chicago

---
+++ src/PizzaStore/Store/TraditionalStore.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using PizzaStore.Pizzas;
+
+namespace PizzaStore.Store
+{
+    public class TraditionalStore : PizzaStore
+    {
+        protected override Pizza CreatePizza(string type)
+        {
+            if (type.Equals("Cheese"))
+            {
+                return new CheesePizza();
+            }
+
+            if (type.Equals("Greek"))
+            {
+                return new GreekPizza();
+            }
+
+            if (type.Equals("Pepperoni"))
+            {
+                return new PepperoniPizza();
+            }
+
+            throw new NotSupportedException(nameof(type));
+        }
+    }
+}
